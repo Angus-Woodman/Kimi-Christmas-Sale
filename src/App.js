a60aeee514d3e2ba4d17fb67602e0f2820897836
Angus Woodman <angus.woodman@outlook.comgit config --global user.name yourGithubUsernameAngus-Woodman>
2020-10-20 16:51:33 +0100

    initial pages setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./styles/App.css";
 import NavBar from "./Components/NavBar"
+import {Home, About, Work, Contact} from "./Pages"
+
 
 class App extends React.Component {
   state = {};
@@ -8,6 +10,10 @@ class App extends React.Component {
     return (
       <>
         <NavBar />
+        <Home />
+        <About />
+        <Work />
+        <Contact />
       </>
     );
   }
